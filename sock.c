4cdfd803a025d321e5844aac8b88114038e5e2a9
Ram Varra <ramvarra@hotmail.co>
2021-02-06 22:53:51 -0700

    code

---
+++ sock.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <string.h>
+#include <linux/socket.h>
+#include <netinet/in.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <unistd.h>
+
+
+#include "sock.h"
+
+void fatal(char *fmt, ...) {
+    char buf[1024];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buf, sizeof(buf), fmt, args);
+    perror(buf);
+    va_end(args);
+    exit(1);
+}
+
+/*
+** Messages are NULL terminated strings.
+*/
+void send_msg(int fd, char *buf) {
+    int n = strlen(buf)+1;
+    int n_sent = 0;
+    int current_sent;
+    while (n_sent < n) {
+        if ((current_sent = write(fd, buf+n_sent, n-n_sent)) < 0) {
+            if (current_sent == 0) {
+                fatal("Unexpected return of 0 sent");
+            }
+            fatal("Failed to write to sock %d bytes", n-n_sent);
+        }
+        n_sent += current_sent;
+    }
+    printf("SND: '%s'\n", buf);
+}
+
+void receive_msg(int fd, char *buf, int buf_size) {
+    int n_received = 0;
+    int c;
+    while (n_received < buf_size-1) {
+        if ((c = read(fd, buf+n_received, 1)) <= 0) {
+            if (c == 0) {
+                break;
+            }
+            fatal("Sock read failed");
+        }
+        n_received += 1;
+        if (buf[n_received - 1] == 0) {
+            break;
+        }
+    }
+    buf[n_received] = 0;
+    printf("RCV: '%s'\n", buf);
+}
+
+void conversation_client(int sock) {
+    char buf[1024];
+    for (int i = 0; i < 10; i++) {
+        sprintf(buf, "HELLO %d", i+1);
+        send_msg(sock, buf);
+        receive_msg(sock, buf, sizeof(buf));
+    }
+}
+
+void conversation_server(int sock) {
+    char buf[1024];
+    int n_read = 0;
+    while (1) {
+        receive_msg(sock, buf, sizeof(buf));
+        if (strlen(buf) == 0) {
+            break;
+        }
+        send_msg(sock, buf);
+    }
+}
+
+void sock_client(char *server) {
+    int client_sock;
+    struct sockaddr_in server_address;
+
+    printf("I am sock client\n");
+    if ((client_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        fatal("Failed to client sock");
+    }
+    MEM_ZERO(server_address);
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(SERVER_PORT);
+ 
+    struct hostent *he = gethostbyname(server);
+    if (he == NULL) {
+        fatal("Can not resolve server %s", server);
+    }
+    memcpy(&server_address.sin_addr, he->h_addr_list[0], sizeof(server_address.sin_addr));
+    
+    printf("Connecting to server: %s port %d\n", server, SERVER_PORT);
+    if (connect(client_sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        fatal("Failed to connect to server");
+    } 
+    conversation_client(client_sock);
+    close(client_sock);
+}
+
+void sock_server() {
+    printf("I am sock server\n");
+    int server_sock;
+    struct sockaddr_in server_address;
+
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        fatal("Failed to create server sock");
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(SERVER_PORT);
+    if (bind(server_sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        fatal("Failed to bind server address to sock");
+    }
+    listen(server_sock, 5);
+    int sock;
+    printf("Start serving on port %d\n", SERVER_PORT);
+    while ((sock = accept(server_sock, (struct sockaddr *)NULL, NULL)) >= 0) {
+        conversation_server(sock);
+        close(sock);
+    }
+    fatal("Failed to accept a connection");
+}
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        printf("Hello arg = %s\n", argv[i]);
+    }
+
+    if (argc == 2) {
+        sock_client(argv[1]);
+    } else {
+        sock_server();
+    } 
+
+    return(0);
+}
---
+++ sock.h
@@ -0,0 +1,6 @@
+#ifndef __SOCK_H__
+#define __SOCK_H__
+
+#define SERVER_PORT 15355
+#define MEM_ZERO(m) memset(&m, 0, sizeof(m))
+#endif
